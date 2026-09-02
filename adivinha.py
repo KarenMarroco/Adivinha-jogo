089941d79382e53d2b5120f37306db2b1797982c
Karen S Marroco <karens.marroco@gmail.com>
2026-09-02 12:44:23 -0300

    Add files via upload

---
+++ adivinha.py
@@ -0,0 +1,27 @@
+print('jogo da adivinhação')
+print('tente adivinhar o número que estou pensando entre 1 e 100')
+import random
+numero_secreto = random.randint(1, 100)
+print('você tem 7 tentativas para acertar o número secreto')
+
+numero_secreto = random.randint(1, 100)
+
+contador = 7
+acertou = False
+
+while contador > 0:
+    tentativa = int(input('Digite o seu palpite: '))
+    if tentativa == numero_secreto:
+        print('Parabéns! Você acertou o número secreto!')
+        acertou = True
+        break
+    elif tentativa < numero_secreto:
+        print('O número secreto é maior do que o seu palpite.')
+    else:
+        print('O número secreto é menor do que o seu palpite.')
+    contador -= 1
+
+if not acertou:
+    print('Que pena! Você errou. O número secreto era:', numero_secreto)
+else:
+    print('Você acertou o número secreto com', 7 - contador + 1, 'tentativas!')
